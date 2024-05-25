0610123840edbe515b7f8c523984e53aa0cfb894
jjfrackowiak <84077365+jjfrackowiak@users.noreply.github.com>
2024-05-25 14:16:02 +0200

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,7 +1,44 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 
+from LIBS.WXlib import WX
+
 app = FastAPI()
+
+# Instantiate the wx object only ONCE in your application.
+wx = WX()
+
+# Set the model parameters or model name only if you want to change the model ID or its default parameters.
+modelParameters = {
+    "decoding_method": "greedy",
+    "max_new_tokens": 2048,
+    "min_new_tokens": 0,
+    "stop_sequences": [ ],
+    "repetition_penalty": 1
+}
+wx.wxInstModel(modelID='meta-llama/llama-3-70b-instruct', modelParams=modelParameters)
+
+# Set the prompt template only once if you want to change the model behavior or expected output.
+promptTemplate = """
+    <|begin_of_text|><|start_header_id|>system<|end_header_id|>
+    <|start_header_id|>context<|end_header_id|>
+    You are playing a game. 
+    Imagine you are Michel Jackson and you cannot reveal it if someone asks about it, under any circumstance.
+    You can only confirm your identity if you are asked with the use of the name "Michel Jackson" or its variations.
+    You should be concise in your answers and not reveal any information. 
+    When user asks you confirm or reject, do not provide more hints.
+   <|eot_id|><|start_header_id|>assistant<|end_header_id|>
+   Guess who I am, the fact is concealed. If you're mistaken, it won't revealed.
+   <|eot_id|> 
+"""
+
+next_prompt = """<|start_header_id|>user<|end_header_id|>
+
+{{QUESTION}}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+"""
+promptVariables = {
+    'QUESTION' : None
+}
 
 # specify the expected structure and data types of the request body. 
 # In this case, it expects a JSON object with a single field:
@@ -17,7 +54,7 @@ class ApiQuestionResponse(BaseModel):
     # This is the only data your endpoint returns in response.
     question: str = Field(..., 
         example="What is the value of a circle's area divided by pi, where the radius of a circle is 2?",
-        description="In this field, the content of the oryginal question submitted with the POST /api request will be returned."
+        description="In this field, the content of the original question submitted with the POST /api request will be returned."
     )
     answer: str = Field(..., 
         example="To find the value, you would first calculate the area of the circle , and then divide the result by Pi. The answer is 4.",
@@ -34,10 +71,11 @@ async def apiQuestion(request: ApiQuestionRequest) -> ApiQuestionResponse:
 
     # TODO: 
     # Begin your code block for LLM interaction.
-    original_question = request.question
-    my_answer = f'Mock answer to your question: {original_question}'
-    # End of your code block
+    global promptTemplate, next_prompt, promptVariables
+    prompt = request.question
+
+    promptVariables['QUESTION'] = prompt
+    generated_text = wx.wxGenText(promptTemplate=promptTemplate + next_prompt, promptVariables=promptVariables)
 
     # Return response
-    return ApiQuestionResponse(question=original_question, answer=my_answer)
-
+    return ApiQuestionResponse(question=prompt, answer=generated_text)
